27bdfc395d967e68c0fca4694f083555c4f72801
Arthurmegon <m.arthur@aluno.edu.br>
2022-04-06 11:27:25 -0300

    atualizando o código para C# 6.0

--- Program.cs
+++ Program.cs
@@ -1,25 +1,14 @@
-﻿using System;
+﻿    Console.WriteLine("Entre com o seu nome");
+    string nome = Console.ReadLine();
 
-namespace CalculadoraIMC
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Entre com o seu nome");
-            string nome = Console.ReadLine();
-
-            Console.WriteLine("Entre com sua altura em metros. Ex:1,70");
-            float altura = Convert.ToSingle(Console.ReadLine());
+    Console.WriteLine("Entre com sua altura em metros. Ex:1,70");
+    float altura = Convert.ToSingle(Console.ReadLine());
 
             
-            Console.WriteLine("Entre com seu peso em Kg. Ex: 102,5");
-            float peso = Convert.ToSingle(Console.ReadLine());
+    Console.WriteLine("Entre com seu peso em Kg. Ex: 102,5");
+    float peso = Convert.ToSingle(Console.ReadLine());
 
-            double imc = peso / (altura * altura);
+    double imc = peso / (altura * altura);
 
-            Console.WriteLine("{0} sua altura é {1} e seu peso {2}", nome, altura, peso);
-            Console.WriteLine("Seu IMC é {0}", imc);
-        }
-    }
-}
+    Console.WriteLine("{0} sua altura é {1} e seu peso {2}", nome, altura, peso);
+    Console.WriteLine("Seu IMC é {0}", imc);
